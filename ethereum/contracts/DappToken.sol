ace2579755ecab0c655790356e9a9d7a01266cef
Ali <humaidan@gmail.com>
2021-06-07 12:57:22 +0300

    delegated transfer

--- ethereum/contracts/DappToken.sol
+++ ethereum/contracts/DappToken.sol
@@ -7,6 +7,19 @@ contract DappToken {
 
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(
+      address indexed _from,
+      address indexed _to,
+      uint256 _value
+    );
+
+    event Approve(
+      address indexed _owner,
+      address indexed _spender,
+      address _value
+    );
 
     function DappToken(uint256 _initialSupply) public {
       name = 'DApp Token';
@@ -17,12 +30,7 @@ contract DappToken {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    event Transfer(
-      address indexed _from,
-      address indexed _to,
-      uint256 _value
-    );
-
+    //Main Transfer function
     function transfer(address _to, uint256 _value) public returns (bool success) {
       //check if account doesn't have enough
       require (balanceOf[msg.sender] >= _value);
@@ -35,6 +43,32 @@ contract DappToken {
       Transfer(msg.sender, _to, _value);
 
       // return bool
+      return true;
+    }
+
+    //Delegated Transfer
+
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+      //allowance
+      allowance[msg.sender][_spender] = _value;
+      Approve(msg.sender, _spender, _value);
+
+      return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+      require(_value <= balanceOf[_from]); //_from has enough tokens
+      require(_value <= allowance[_from][msg.sender]); // allowance is big enough
+
+      // change balance
+      balanceOf[_from] -= _value;
+      balanceOf[_to] += _value;
+
+      allowance[_from][msg.sender] -= _value;
+
+      Transfer (_from, _to, _value);
+
       return true;
     }
 }
